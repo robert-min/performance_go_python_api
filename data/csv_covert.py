9b560af19198ec9a04179483c8dd729b455092d1
robert-min <robertmin522@gmail.com>
2023-06-10 13:47:32 +0900

    fix: conf

--- data/csv_covert.py
+++ data/csv_covert.py
@@ -1,5 +1,8 @@
 import csv
 import requests
+import time
+import json
+
 file = open("./origin/ratings.csv", "r")
 all_ratings = csv.reader(file)
 next(all_ratings)
@@ -10,10 +13,10 @@ for idx, row in enumerate(all_ratings):
         break
     requests.post("http://localhost:8080/movies", 
                     headers={"Content-Type": "application/json"},
-                    data={
+                    data=json.dumps({
                         "name": row[0],
                         "movie_id": row[1],
                         "rating": row[2],
                         "timestamp": row[3]
-                    })
-    
+                    }))
+    time.sleep(0.001)
